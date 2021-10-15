11ee4f7c577cd6030ad9673b1c5a96b52b48ebd8
kogahi <h.koga22@icloud.com>
2021-10-15 22:19:14 +0900

    node課題①-2

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 
 // テンプレートエンジンの指定
 app.set("view engine", "ejs");
 
-app.get("/", function (req, res) {
-    // レンダリングを行う
-    res.render("./login.ejs");
-});
+// app.get("/", function (req, res) {
+//     // レンダリングを行う
+//     res.render("./login.ejs");
+// });
 
-app.get("/regist", function (req, res) {
-    // レンダリングを行う
-    res.render("./regist.ejs");
-});
+// app.get("/regist", function (req, res) {
+//     // レンダリングを行う
+//     res.render("./regist.ejs");
+// });
+
+let loginRouter = require('./routes/login');
+app.use('/', loginRouter);
+
+let registRouter = require('./routes/regist');
+app.use('/regist', registRouter);
 
 app.listen(3000);

2aa79ccde2b0ba045433c1f7b9134b539b535732
kogahi <h.koga22@icloud.com>
2021-10-27 22:10:53 +0900

    node②-2

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const { validationResult } = require('express-validator');
 const Views = '../views/'
 
 module.exports = {
     getLoggedin: function(req, res) {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+         res.render('regist',
+            {name: req.body.name,
+            password: req.body.password,
+            cfmPassword: req.body.cfmPassword,
+            errors: errors.errors});
+        }
         res.render(Views + 'home.ejs', {req:req});
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -3,36 +3,10 @@ const { check, validationResult } = require('express-validator');
 const loginController = require('../controllers/loginController')
 const registController = require('../controllers/registController')
 const homeController = require('../controllers/homeController')
+const validator = require('../validates/validate')
 
 router.get('/', loginController.getLogin)
 router.get('/regist', registController.getRegist)
-// router.post('/home', homeController.getLoggedin)
-router.post('/home',  [
-    check('name').not().isEmpty().withMessage('名前を入力してください。'),
-    check('password').not().isEmpty().withMessage('パスワードを入力してください。'),
-    check('password').isLength({ min: 7 }).withMessage('パスワードは7文字以上で設定してください。'),
-    check('cfmPassword').
-     custom((value, {req}) => {
-         if(req.body.password === req.body.cfmPassword) {
-             return true;
-         }
-     }).withMessage('確認用パスワードが不一致です。')
-],
-(req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.render('regist',
-      {name: req.body.name,
-       password: req.body.password,
-       cfmPassword: req.body.cfmPassword,
-       errors: errors.errors});
-    }
-    homeController.getLoggedin(req,res)
-    // ({
-    //     username: req.body.username,
-    //     password: req.body.password,
-    //     cfmPassword: req.body.cfmPassword
-    //   });
-})
+router.post('/home', validator, homeController.getLoggedin)
 
 module.exports = router;
